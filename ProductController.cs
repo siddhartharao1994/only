e2d1f4566d2d306239a28e52468e8770f908619e
siddhartharao1994 <50092864+siddhartharao1994@users.noreply.github.com>
2019-07-01 02:10:24 +0530

    Add files via upload

---
+++ ProductController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BLL;
+namespace EcoomerceMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult validate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult validate(string username,string password)
+        {
+            bool status = Class1.Validate(username, password);
+            if(status)
+            {
+                return RedirectToAction("Index", "Product");
+            }
+            return View();
+
+        }
+    }
+}
---
+++ ecomDAL.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class ecomDAL
+    {
+        public static bool Validate(string username, string password)
+        {
+            bool status = false;
+            SqlConnection conn = new SqlConnection();
+            SqlCommand cmd = new SqlCommand();
+            string connstr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\USERS\USER105\SOURCE\REPOS\MVC\ECOOMERCEMVC\APP_DATA\DATABASE1.MDF;Integrated Security=True";
+            conn.ConnectionString = connstr;
+            string qry = "select * from sid where Username=@uname and Password=@pwd";
+
+            cmd.CommandText = qry;
+            cmd.Parameters.Add(new SqlParameter("@uname", username));
+            cmd.Parameters.Add(new SqlParameter("@pwd", password));
+            cmd.Connection = conn;
+            try
+            {
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    status = true;
+                }
+                reader.Close();
+            }
+            catch (SqlException e)
+            {
+                throw e;
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return status;
+        }
+    }
+}
